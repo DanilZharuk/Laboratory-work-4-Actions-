8a9b22590ffd75eb9e6bab7d1444f8bfdcb7c384
DanilZharuk <162281384+DanilZharuk@users.noreply.github.com>
2025-10-21 15:44:40 +0300

    Create ProgramCode.cs

---
+++ ProgramCode.cs
@@ -0,0 +1,13 @@
+// ProgramCode.cs
+using System;
+
+namespace RelaxationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Програмний продукт: Relaxation System");
+        }
+    }
+}
